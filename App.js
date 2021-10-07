3a214b2dca289378288ad1802cab8a73b2938224
Robson Quaresma <robquaresma@gmail.com>
2021-10-07 20:49:18 -0300

    passo #1

--- App.js
+++ App.js
@@ -70,7 +70,7 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
+          <Section title="Passo #1">
             Edit My new <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
